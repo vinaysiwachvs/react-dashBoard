7f968e328a0e627a0bb0f10ee8e99458e29a4e1e
Vinay Siwach <56073955+vinaysiwachvs@users.noreply.github.com>
2023-07-15 20:27:01 +0530

    adding route and navbar

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className='footer'>Footer</div>
+  )
+}
+
+
+export default Footer
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,10 @@
+import './menu.scss'
+
+const Menu = () => {
+  return (
+    <div className='menu'>Menu</div>
+  )
+}
+
+
+export default Menu
