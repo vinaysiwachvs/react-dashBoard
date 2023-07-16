6c80d1485f8c4eccdbebaebc64e0822be5ed5853
Vinay Siwach <56073955+vinaysiwachvs@users.noreply.github.com>
2023-07-16 11:12:56 +0530

    single user or product

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,67 @@
 import Home from "./pages/home/Home";
-import Users from "./pages/user/Users";
+import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
 import Navbar from "./components/navbar/Navbar";
 import Menu from "./components/menu/Menu";
 import Footer from "./components/footer/Footer";
 import Login from "./pages/login/Login";
-import {createBrowserRouter,RouterProvider,Outlet,} from "react-router-dom";
-import './styles/global.scss'
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import "./styles/global.scss";
+import Product from "./pages/product/Product";
+import User from "./pages/user/User";
 
 function App() {
+	const Layout = () => {
+		return (
+			<div className='main'>
+				<Navbar />
+				<div className='container'>
+					<div className='menuContainer'>
+						<Menu />
+					</div>
+					<div className='contentContainer'>
+						<Outlet />
+					</div>
+				</div>
+				<Footer />
+			</div>
+		);
+	};
 
-  const Layout = () => {
-    return(
-      <div className="main">
-        <Navbar/>
-        <div className="container">
-          <div className="menuContainer">
-            <Menu/>
-          </div>
-          <div className="contentContainer">
-            <Outlet/>
-          </div>
-        </div>
-        <Footer/>
-      </div>
-    )
-  }
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Layout />,
+			children: [
+				{
+					path: "/",
+					element: <Home />,
+				},
+				{
+					path: "users",
+					element: <Users />,
+				},
+				{
+					path: "products",
+					element: <Products />,
+				},
+				{
+					path: "users/:id",
+					element: <User />,
+				},
+				{
+					path: "products/:id",
+					element: <Product />,
+				},
+			],
+		},
+		{
+			path: "/login",
+			element: <Login />,
+		},
+	]);
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Layout/>,
-    children:[
-      {
-        path:'/',
-        element: <Home/>
-      },
-      {
-        path:'users',
-        element: <Users/>
-      },
-      {
-        path:'products',
-        element: <Products/>
-      },
-    ]
-  },
-  {
-    path:'/login',
-    element:<Login/>
-  }
-]);
-
-  return (
-    <RouterProvider router={router} />
-  )
+	return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
